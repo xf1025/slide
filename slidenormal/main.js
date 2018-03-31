bd1128c90b5af5263dec2b12e5ae0f9d7cf6de56
mark <1025309512@qq.com>
2018-03-31 20:33:31 +0800

    js的重构

--- slidenormal/main.js
+++ slidenormal/main.js
@@ -2,16 +2,13 @@ var allButtons = $('#buttons > span')
 
 for (let i = 0; i < allButtons.length; i++) {
   $(allButtons[i]).on('click', function(x) {
-    console.log('hi')
     var index = $(x.currentTarget).index()
     var p = index * -300
     $('#images').css({
       transform: 'translate(' + p + 'px)'
     })
     n = index
-    allButtons.eq(n)
-      .addClass('red')
-      .siblings('.red').removeClass('red')
+    activeButton(allButtons.eq(n))
   })
 }
 
@@ -20,25 +17,27 @@ for (let i = 0; i < allButtons.length; i++) {
 var n = 0;
 var size = allButtons.length
 allButtons.eq(n % size).trigger('click')
-  .addClass('red')
-  .siblings('.red').removeClass('red')
 
-var timerId = setInterval(() => {
-  n += 1
-  allButtons.eq(n % size).trigger('click')
+
+var timerId = setTimer()
+
+function setTimer() {
+  return setInterval(() => {
+    n += 1
+    allButtons.eq(n % size).trigger('click')
+  }, 3000)
+}
+
+function activeButton($button) {
+  $button
     .addClass('red')
     .siblings('.red').removeClass('red')
-}, 1000)
+}
 
 $('.window').on('mouseenter', function() {
   window.clearInterval(timerId)
 })
 
 $('.window').on('mouseleave', function() {
-  timerId = setInterval(() => {
-    n += 1
-    allButtons.eq(n % size).trigger('click')
-      .addClass('red')
-      .siblings('.red').removeClass('red')
-  }, 3000)
+  timerId = setTimer()
 })
